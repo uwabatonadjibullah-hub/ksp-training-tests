c6b60deed73bc08efb8dd41b6fa815743d015ec9
Uwabatonadjibullah-hub <uwabatonadjibullah@gmail.com>
2025-08-16 07:40:33 +0200

    ALL IN AUG 16

--- src/pages/UpcomingQuizzes.jsx
+++ src/pages/UpcomingQuizzes.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { db } from '../firebase';
 import { collection, query, where, getDocs } from 'firebase/firestore';
-import './upcoming.css'; // optional styling
+import 'UpcomingQuizzes.css'; // optional styling
 
 function UpcomingQuizzes() {
   const [quizzes, setQuizzes] = useState([]);
