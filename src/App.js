c6c903d93cebce36098fa6e2174aedc4a9483811
Uwabatonadjibullah-hub <uwabatonadjibullah@gmail.com>
2025-08-19 16:06:35 +0200

    ALL IN AUG 19

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from './react-router-dom';
 
 import logo1 from './logo.png';
 import logo2 from './ksp-logo.jpg';
